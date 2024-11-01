99fac8c537c24fc4b41cd159df82f89e2fc660ae
charlesanthony1996 <charlesanthony296@gmail.com>
2024-11-01 18:14:06 +0100

    some indicator comps

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,35 @@
-import * as ReactNative from "react-native"
+import { Text, View, StyleSheet } from "react-native"
 import React from "react"
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
+import { ActivityIndicator } from "react-native-paper"
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  }
+})
 
 
 function App() {
   return (
-  <ReactNative.View>
-    <ReactNative.Text>This is my way of showing things</ReactNative.Text>
-  </ReactNative.View>
+    <SafeAreaProvider>
+      <SafeAreaView style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator></ActivityIndicator>
+        <Text>This is my way of showing things</Text>
+        <ActivityIndicator size="large"></ActivityIndicator>
+        <ActivityIndicator size="small" color="grey"></ActivityIndicator>
+        <ActivityIndicator size="large" color="green"></ActivityIndicator>
+        <ActivityIndicator size="large" color="black"></ActivityIndicator>
+    </SafeAreaView>
+    </SafeAreaProvider>
   )
-
 }
 
 export default App
