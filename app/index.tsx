e4404f3ebabcdbc4fa0df3c4ea2bae6c15a16f35
charlesanthony1996 <charlesanthony296@gmail.com>
2024-11-01 17:06:54 +0100

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,59 +1,14 @@
-import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Text, View, StyleSheet, RefreshControl, ScrollView } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as ReactNative from "react-native"
+import React from "react"
 
-function ScreenA() {
+
+function App() {
   return (
-    <View>
-      <Text>First Item</Text>
-    </View>
-  );
+  <ReactNative.View>
+    <ReactNative.Text>This is my way of showing things</ReactNative.Text>
+  </ReactNative.View>
+  )
+
 }
 
-function ScreenB() {
-  return (
-    <View>
-      <Text>Second Item</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  scrollView: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-const Drawer = createDrawerNavigator();
-
-export default function App() {
-  const [refreshing, setRefreshing] = React.useState(false);
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 2000);
-  }, []);
-
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <ScrollView
-          contentContainerStyle={styles.scrollView}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }>
-          <Text>Pull down to see RefreshControl indicator</Text>
-        </ScrollView>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
-}
+export default App
