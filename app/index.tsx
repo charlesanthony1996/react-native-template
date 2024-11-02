d18531e365f1999710d3a1cac0cf8bf0a908a8e5
charlesanthony1996 <charlesanthony296@gmail.com>
2024-11-02 17:12:35 +0100

    switch component

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,34 @@
-import React, { useEffect, useState } from "react"
-import { StyleSheet, Text, TouchableHighlight, View} from "react-native"
+import React, { useState} from "react"
+import { Switch, StyleSheet } from "react-native"
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
 
 
 
 function App() {
+  const [isEnabled, setIsEnabled] = useState(false)
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState )
 
-  const [count, setCount] = useState(0)
-  const onPress = () => setCount(count + 1)
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <TouchableHighlight onPress={onPress}>
-          <View style={styles.button}>
-            <Text>Touch here</Text>
-          </View>
-        </TouchableHighlight>
-        <View style={styles.countContainer}>
-          <Text style={styles.countText}>{count || null}</Text>
-        </View>
+        <Switch
+        ios_backgroundColor={'none'}
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+        >
+        </Switch>
+
       </SafeAreaView>
     </SafeAreaProvider>
   )
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 10
-  },
-  button: {
     alignItems: 'center',
-    backgroundColor: '#dddddd',
-    padding: 10
-  },
-  countContainer: {
-    alignItems: 'center',
-    padding: 10
-  },
-  countText: {
-    color: '#ff00ff'
+    justifyContent: 'center'
   }
 })
 
