c2aa8e574ba48a35958bb0b11186206d76fb2221
charlesanthony1996 <charlesanthony296@gmail.com>
2024-11-02 11:31:52 +0100

    touchable highlight comp

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,28 @@
-import React from "react";
-import { 
-  View, 
-  KeyboardAvoidingView, 
-  TextInput, 
-  StyleSheet, 
-  Text, 
-  Platform, 
-  TouchableWithoutFeedback, 
-  Button, 
-  Keyboard } from 
-"react-native"
-
+import React from "react"
+import { StyleSheet, Text, TouchableHighlight, View} from "react-native"
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
 
 function App() {
   return (
-    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inner}>
-          <Text style={styles.inner}>Header</Text>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    <SafeAreaProvider>
+      <SafeAreaView>
+
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  inner: {
-    padding: 24,
     flex: 1,
-    justifyContent: 'space-around'
+    justifyContent: 'center',
+    paddingHorizontal: 10
+  },
+  button: {
+    alignItems: 'center',
+    
   }
+
 })
 
 export default App
