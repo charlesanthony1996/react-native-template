2689845e480ddddeb56f49eed64be57607277618
charlesanthony1996 <charlesanthony296@gmail.com>
2024-11-02 16:58:21 +0100

    count component

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { StyleSheet, Text, TouchableHighlight, View} from "react-native"
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
 
+
+
 function App() {
+
+  const [count, setCount] = useState(0)
+  const onPress = () => setCount(count + 1)
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-
+      <SafeAreaView style={styles.container}>
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button}>
+            <Text>Touch here</Text>
+          </View>
+        </TouchableHighlight>
+        <View style={styles.countContainer}>
+          <Text style={styles.countText}>{count || null}</Text>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   )
@@ -20,9 +32,16 @@ const styles = StyleSheet.create({
   },
   button: {
     alignItems: 'center',
-    
+    backgroundColor: '#dddddd',
+    padding: 10
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10
+  },
+  countText: {
+    color: '#ff00ff'
   }
-
 })
 
 export default App
