9d1ca296bf6f6de25497dce2b234af883579fbdc
charlesanthony1996 <charlesanthony296@gmail.com>
2024-11-02 10:44:32 +0100

    keyboard avoiding view comp

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,40 @@
-import { Text, View, StyleSheet } from "react-native"
-import React from "react"
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
-import { ActivityIndicator } from "react-native-paper"
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10
-  }
-})
+import React from "react";
+import { 
+  View, 
+  KeyboardAvoidingView, 
+  TextInput, 
+  StyleSheet, 
+  Text, 
+  Platform, 
+  TouchableWithoutFeedback, 
+  Button, 
+  Keyboard } from 
+"react-native"
 
 
 function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={[styles.container, styles.horizontal]}>
-        <ActivityIndicator></ActivityIndicator>
-        <Text>This is my way of showing things</Text>
-        <ActivityIndicator size="large"></ActivityIndicator>
-        <ActivityIndicator size="small" color="grey"></ActivityIndicator>
-        <ActivityIndicator size="large" color="green"></ActivityIndicator>
-        <ActivityIndicator size="large" color="black"></ActivityIndicator>
-    </SafeAreaView>
-    </SafeAreaProvider>
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.inner}>Header</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: 'space-around'
+  }
+})
 
 export default App
